e84db06fb8ed437df29ce6a338916e5f815b6c9d
maedana <maeda.na@gmail.com>
2022-05-02 10:02:39 +0900

    lで移動出来るところまで

--- app/javascript/controllers/todos_controller.js
+++ app/javascript/controllers/todos_controller.js
@@ -16,4 +16,14 @@ export default class extends Controller {
     const focusedTodo = this.element.querySelector('.js-todo:focus')
     focusedTodo.previousElementSibling.focus()
   }
+
+  moveRight() {
+    const focusedTodo = this.element.querySelector('.js-todo:focus')
+    const todoLists = this.element.querySelectorAll('.js-todos')
+    const currentTodoList = focusedTodo.closest('.js-todos')
+    const currentTodoListIndex = Array.from(todoLists).findIndex((item) => item === currentTodoList)
+    const nextTodoList = todoLists[currentTodoListIndex + 1] || todoLists[0]
+    const nextFocusTodo = nextTodoList.querySelector('.js-todo')
+    nextFocusTodo.focus()
+  }
 }
