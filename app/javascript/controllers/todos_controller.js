41839a2b78f23dff39e2412fa8574f1c495c5087
maedana <maeda.na@gmail.com>
2022-05-04 16:18:23 +0900

    js-todosを使わずにStimulusのTargetを使うようにリファクタ

--- app/javascript/controllers/todos_controller.js
+++ app/javascript/controllers/todos_controller.js
@@ -2,10 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="todos"
 export default class extends Controller {
+  static targets = ['todoList']
+
   connect() {
     this.focusedElementsByTodos = []
     const firstTodo = this.element.querySelector('.js-todo')
     firstTodo.focus()
+    this.currentTodoListIndex = 0
   }
 
   moveDown() {
@@ -20,28 +23,22 @@ export default class extends Controller {
 
   moveRight() {
     const focusedTodo = this.element.querySelector('.js-todo:focus')
-    const todoLists = this.element.querySelectorAll('.js-todos')
-    const currentTodoList = focusedTodo.closest('.js-todos')
-    const currentTodoListIndex = Array.from(todoLists).findIndex((item) => item === currentTodoList)
-    this.focusedElementsByTodos[currentTodoListIndex] = focusedTodo
-    let nextFocusTodo = this.focusedElementsByTodos[currentTodoListIndex + 1] || todoLists[currentTodoListIndex + 1]?.querySelector('.js-todo')
-    if (!nextFocusTodo) {
-      nextFocusTodo = this.focusedElementsByTodos[0] || todoLists[0]?.querySelector('.js-todo')
-    }
+    const todoLists = this.todoListTargets
+    this.focusedElementsByTodos[this.currentTodoListIndex] = focusedTodo
+    const nextTodoListIndex = todoLists[this.currentTodoListIndex + 1] ? this.currentTodoListIndex + 1 : 0
+    let nextFocusTodo = this.focusedElementsByTodos[nextTodoListIndex] || todoLists[nextTodoListIndex]?.querySelector('.js-todo')
+    this.currentTodoListIndex = nextTodoListIndex
     nextFocusTodo.focus()
   }
 
   // MEMO: moveRightとほぼ同じDRYにしたいところ
   moveLeft() {
     const focusedTodo = this.element.querySelector('.js-todo:focus')
-    const todoLists = this.element.querySelectorAll('.js-todos')
-    const currentTodoList = focusedTodo.closest('.js-todos')
-    const currentTodoListIndex = Array.from(todoLists).findIndex((item) => item === currentTodoList)
-    this.focusedElementsByTodos[currentTodoListIndex] = focusedTodo
-    let nextFocusTodo = this.focusedElementsByTodos[currentTodoListIndex - 1] || todoLists[currentTodoListIndex - 1]?.querySelector('.js-todo')
-    if (!nextFocusTodo) {
-      nextFocusTodo = this.focusedElementsByTodos[todoLists.length - 1] || todoLists[todoLists.length - 1]?.querySelector('.js-todo')
-    }
+    const todoLists = this.todoListTargets
+    this.focusedElementsByTodos[this.currentTodoListIndex] = focusedTodo
+    const nextTodoListIndex = todoLists[this.currentTodoListIndex - 1] ? this.currentTodoListIndex - 1 : todoLists.length - 1
+    let nextFocusTodo = this.focusedElementsByTodos[nextTodoListIndex] || todoLists[nextTodoListIndex]?.querySelector('.js-todo')
+    this.currentTodoListIndex = nextTodoListIndex
     nextFocusTodo.focus()
   }
 
